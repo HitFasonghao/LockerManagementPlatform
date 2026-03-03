16d13c8349ad293f2fdb2940f89919600c83e136
songhao <xiwei_268@qq.com>
2026-03-03 23:05:58 +0800

    同步进度

--- src/views/login/api.js
+++ src/views/login/api.js
@@ -6,4 +6,5 @@ export default {
   loginByPassword: data => request.post('/login/loginByPassword', data, { needToken: false }),
   loginBySmsCode: data => request.post('/login/loginBySmsCode', data, { needToken: false }),
   sendSmsCode: data => request.post('/login/smsCode', data, { needToken: false }),
+  getCaptcha: () => request.get('/login/captcha', { needToken: false }),
 }
