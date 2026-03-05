f0ca9a8a124419c8cfc649e172ffd117e9a26bf0
fasonghao <xiwei_268@qq.com>
2026-03-05 16:31:25 +0800

    关闭eslint插件对未使用变量的检查

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,8 @@ export default antfu({
     'vue/block-order': 'off',
     '@typescript-eslint/no-this-alias': 'off',
     'prefer-promise-reject-errors': 'off',
+    'no-unused-vars': 'off', // 关闭 ESLint 的 no-unused-vars 规则，交由 eslint-plugin-unused-imports 处理未使用的变量和导入
+    'unused-imports/no-unused-vars': 'off', // 关闭 eslint-plugin-unused-imports 的 no-unused-vars 规则，交由 ESLint 处理未使用的变量（但不处理未使用的导入）
   },
   languageOptions: {
     globals: {
